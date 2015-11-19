bdb244829fb45a366f468a8faa8b9015413c4323
bbouley <b.bouley@hotmail.co.uk>
2015-11-18 17:11:37 -0700

    test successfully adding funds to site acc

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -4,13 +4,4 @@ Stripe.setPublishableKey('pk_test_vT4aH91XlPiEJhU79u6Tktxz');
 
 $(document).on('ready', function() {
 
-    $('.stripe-connect').on('click', function(e) {
-        e.preventDefault();
-
-        $.get('/authorize', function(data) {
-            console.log(data);
-        })
-
-    })
-
 });
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -9,45 +9,19 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
     var stripeToken = req.body.stripeToken;
-    var charge = stripe.char
+    var charge = stripe.charges.create({
+        amount: 1000, // amount in cents, again
+        currency: "usd",
+        source: stripeToken,
+        description: "Example charge"
+    }, function(err, charge) {
+        if (err && err.type === 'StripeCardError') {
+          res.json('card error');
+        } else {
+            console.log()
+            res.json('much success');
+        }
+    });
 })
-
-router.get('/success', function(req, res, next) {
-    res.render('index', {'title' : 'Success'})
-})
-
-router.get('/authorize', function(req, res) {
-  // Redirect to Stripe /oauth/authorize endpoint
-  res.redirect('https://connect.stripe.com/oauth/authorize' + '?' + qs.stringify({
-    response_type: 'code',
-    scope: 'read_write',
-    client_id: 'ca_7JkneDyQ6WBN1XzqVtlP3YXjcJFv1JOK'
-  }));
-});
-
-router.get('/oauth/callback', function(req, res) {
-
-  var code = req.query.code;
-
-  // Make /oauth/token endpoint POST request
-  request.post({
-    url: TOKEN_URI,
-    form: {
-      grant_type: 'authorization_code',
-      client_id: CLIENT_ID,
-      code: code,
-      client_secret: API_KEY
-    }
-  }, function(err, r, body) {
-
-    var accessToken = JSON.parse(body).access_token;
-
-    // Do something with your accessToken
-
-    // For demo"s sake, output in response:
-    res.send({ 'Your Token': accessToken });
-
-  });
-});
 
 module.exports = router;
