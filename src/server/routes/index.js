30f9e4e3b238903d286ebdb01ac3970fe8c60a61
bbouley <b.bouley@hotmail.co.uk>
2015-11-19 13:09:52 -0700

    added in user creation on route to test

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -4,19 +4,28 @@ var router = express.Router();
 var stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Admin Send Stripe Payments' });
+    res.render('index', { title: 'Admin Send Stripe Payments' });
 });
 
 router.post('/', function(req, res, next) {
+    console.log(req.body.stripeToken);
+
+    stripe.customers.create({
+        description : 'Customer for test@example.com',
+        source      : "tok_178er7Ffzwl6D33Dws1rTNEj"
+        }, function(err, customer) {
+          // asynchronously called
+        });
+
     var stripeToken = req.body.stripeToken;
     var charge = stripe.charges.create({
-        amount: 1000, // amount in cents, again
-        currency: "usd",
-        source: stripeToken,
-        description: "Example charge"
+        amount      : 2000, // amount in cents, again
+        currency    : "usd",
+        source      : stripeToken,
+        description : "Example charge"
     }, function(err, charge) {
         if (err && err.type === 'StripeCardError') {
-          res.json('card error');
+            res.json('card error');
         } else {
             console.log()
             res.json('much success');
